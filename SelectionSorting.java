fdff9cb42cd89fd8a20513f4bac322670cf9b0d8
Jeff <user@USERs-MacBook-Air.local>
2020-09-05 15:57:48 +0300

    [Feature] add change

--- SelectionSorting.java
+++ SelectionSorting.java
@@ -3,7 +3,9 @@ import java.util.Arrays;
 public class SelectionSorting {
     public static void main(String[] args) {
         int[] result = solution(new int[]{1, 2, 3, 1, 4, 1});
+        int[] result2 = solutionIndex(new int[]{1, 2, 3, 1, 4, 1});
         System.out.println("Value = " + Arrays.toString(result));
+        System.out.println("Value 2 = " + Arrays.toString(result2));
     }
 
     private static int[] solution(int[] A){
@@ -25,6 +27,21 @@ public class SelectionSorting {
             
         }
 
+        return A;
+    }
+
+    private static int[] solutionIndex(int[] A){
+        for(int i = 0; i < A.length - 1; i++){
+            int minIndx = i;
+            for(int j = i + 1; j < A.length; j++){
+                if(A[j] < A[minIndx]){
+                    minIndx = j;
+                }
+            }
+            int minVal = A[minIndx];
+            A[minIndx] = A[i];
+            A[i] = minVal; 
+        }
         return A;
     }
 }
