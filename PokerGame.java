2f9d120b2768e0137ab547ec691936c9600f29ac
Jeff <user@USERs-MacBook-Air.local>
2020-09-17 15:49:08 +0300

    [Feature] add change

--- PokerGame.java
+++ PokerGame.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * Poker Game
@@ -32,16 +33,68 @@ the next highest cards are compared, and so on.
  */
 public class PokerGame {
     public static void main(String[] args) {
-        String[] hand1 = new String[]{"TC", "JD", "QH", "KS", "AS"};
-        String[] hand2 = new String[]{"2C", "5C", "2C", "5C", "2C"};
+        String[] hand1 = new String[]{"TC", "JD", "5H", "KS", "AS"};
+        String[] hand2 = new String[]{"2C", "AC", "5C", "8C", "JC"};
         
         // int value = determineWinner(hand1, hand2);
-        // System.out.println(value);
+        // System.out.println("Winner is player " + value);
 
-        isFullHouse(hand2);
+        highestValue(hand2);
+    }
+
+    private static int determineWinner(String[] hand1, String[] hand2){
+        int winner = 0;
+        System.out.println("Player one = " + Arrays.toString(hand1));
+        System.out.println("Player two = " + Arrays.toString(hand2));
+
+        if(isRoyalFlush(hand1)){
+            return 1;
+        } else if (isRoyalFlush(hand2)){
+            return 2;
+        } else if (isStraightFlush(hand1)){
+            return 1;
+        } else if (isStraightFlush(hand2)){
+            return 2;
+        } else if (isFourOfAkind(hand1)){
+            return 1;
+        } else if (isFourOfAkind(hand2)){
+            return 2;
+        } else if (isFullHouse(hand1)){
+            return 1;
+        } else if (isFullHouse(hand2)){
+            return 2;
+        } else if (isFlush(hand1)){
+            return 1;
+        } else if (isFlush(hand2)){
+            return 2;
+        } else if (isStraight(hand1)){
+            return 1;
+        } else if (isStraight(hand2)){
+            return 2;
+        } else if (isThreeOfAKind(hand1)){
+            return 1;
+        } else if (isThreeOfAKind(hand2)){
+            return 2;
+        } else if (isTwoPairs(hand1)){
+            return 1;
+        } else if (isTwoPairs(hand2)){
+            return 2;
+        } else if (isOnePair(hand1)){
+            return 1;
+        } else if (isOnePair(hand2)){
+            return 2;
+        } else if (highestValue(hand2) > highestValue(hand1)){
+            return 2;
+        } else if (highestValue(hand1) > highestValue(hand2)){
+            return 1;
+        }
+
+        return winner;
     }
 
     private static String[] sortHand(String[] hand){
+        TreeMap<Integer, String> treeMap = new TreeMap<>();
+
         for(int i = 0; i < hand.length; i++){
             switch(hand[i].charAt(0)){
                 case 'T':
@@ -62,30 +115,19 @@ public class PokerGame {
                 default:
                     break;
             }
+
+            int key = Integer.parseInt(hand[i].substring(0, hand[i].length()-1));
+            String value = hand[i].substring(hand[i].length() - 1);
+            treeMap.put(key, value);
         }
-        Arrays.sort(hand);
+
+        int index = 0;
+        for(Map.Entry<Integer, String> entry: treeMap.entrySet()){
+            hand[index] = entry.getKey().toString() + entry.getValue();
+            index++;
+        }
 
         return hand;
-    }
-
-    private static int determineWinner(String[] hand1, String[] hand2){
-        int winner = 0;
-
-        if(isRoyalFlush(hand1)){
-            return 1;
-        } else if (isRoyalFlush(hand2)){
-            return 2;
-        } else if (isStraightFlush(hand1)){
-            return 1;
-        } else if (isStraightFlush(hand2)){
-            return 2;
-        } else if (isFourOfAkind(hand1)){
-            return 1;
-        } else if (isFourOfAkind(hand2)){
-            return 2;
-        }
-
-        return winner;
     }
 
     private static boolean isRoyalFlush(String[] hand) {
@@ -110,8 +152,6 @@ public class PokerGame {
         if(royalFlushCount == 5){
             isRoyalFlush = true;
         }
-
-        System.out.println(handString);
 
         return isRoyalFlush;
     }
@@ -159,13 +199,11 @@ public class PokerGame {
 
     private static boolean isFullHouse(String[] input){
         boolean isFullHouse = false;
-        System.out.println("Array = " + Arrays.toString(input));
 
         Map<String, Integer> values = new HashMap<String, Integer>();
         for (String string : input) {
             
             String value = string.substring(0, string.length() - 1);
-            System.out.println(value);
 
             if(values.containsKey(value)){
                 values.replace(value, values.get(value)+1);
@@ -173,14 +211,134 @@ public class PokerGame {
                 values.put(value, 1);
             }
         }
-        System.out.println("Map = " + values.toString());
 
         if(values.containsValue(3) && values.containsValue(2)){
             isFullHouse = true;
         }
 
-        System.out.println("Is full house = " + isFullHouse);
-
         return isFullHouse;
+    }
+
+    private static boolean isFlush(String[] input){
+        boolean isFlush = false;
+        int flushCount = 1;
+
+        for (int i = 0; i < input.length - 1; i++) {
+            if(input[i].charAt(input[i].length() - 1) == input[i+1].charAt(input[i+1].length() - 1)){
+                flushCount++;
+            }
+        }
+
+        if(flushCount == 5){
+            isFlush = true;
+        }
+
+        return isFlush;
+    }
+
+    private static boolean isStraight(String[] input){
+        boolean isStraight = false;
+        int isStraightCount = 1;
+        String[] sortedArr = sortHand(input);
+
+        for(int i = 0; i < sortedArr.length - 1; i++){
+            int value1 = Integer.parseInt(sortedArr[i].substring(0, sortedArr[i].length() - 1));
+            int value2 = Integer.parseInt(sortedArr[i+1].substring(0, sortedArr[i+1].length() - 1));
+
+            if((value2 - value1) == 1){
+                isStraightCount++;
+            }
+        }
+
+        if(isStraightCount == 5){
+            isStraight = true;
+        }
+
+        return isStraight;
+    }
+
+    private static boolean isThreeOfAKind(String[] input){
+        boolean isThreeOfKind = false;
+
+        Map<String, Integer> values = new HashMap<>();
+
+        for (String string : input) {
+            String value = string.substring(0, string.length()-1);
+            if(values.containsKey(value)){
+                values.replace(value, values.get(value) + 1);
+            } else {
+                values.put(value, 1);
+            }
+        }
+
+        if(values.containsValue(3)){
+            isThreeOfKind = true;
+        }
+
+        return isThreeOfKind;
+    }
+
+    private static boolean isTwoPairs(String[] input){
+        boolean isTwoPairs = false;
+        int isTwoPairsCount = 0;
+
+        Map<String, Integer> values = new HashMap<>();
+        for (String string : input) {
+            String value = string.substring(0, string.length()-1);
+            if(values.containsKey(value)){
+                values.replace(value, values.get(value) + 1);
+            } else {
+                values.put(value, 1);
+            }
+        }
+
+        for (Map.Entry<String, Integer> entry : values.entrySet()) {
+            if(entry.getValue() == 2){
+                isTwoPairsCount++;
+            }
+        }
+        System.out.println(isTwoPairsCount);
+
+        if(isTwoPairsCount == 2){
+            isTwoPairs = true;
+        }
+
+        return isTwoPairs;
+    }
+
+    private static boolean isOnePair(String[] input){
+        boolean isOnePair = false;
+
+        Map<String, Integer> values = new HashMap<>();
+        for (String string : input) {
+            String value = string.substring(0, string.length()-1);
+            if(values.containsKey(value)){
+                values.replace(value, values.get(value) + 1);
+            } else {
+                values.put(value, 1);
+            }
+        }
+
+        if(values.containsValue(2) && values.size() > 3){
+            isOnePair = true;
+        }
+        System.out.println(values);
+        System.out.println("Has one pair = " + isOnePair);
+
+        return isOnePair;
+    }
+
+    private static int highestValue(String[] input){
+        System.out.println("Array = " + Arrays.toString(input));
+        String[] sortedHand = sortHand(input);
+
+        System.out.println("Sorted array = " + Arrays.toString(sortedHand));
+
+        for(int i = 0; i < sortedHand.length; i++){
+            sortedHand[i] = sortedHand[i].substring(0, sortedHand[i].length() - 1);
+        }
+        System.out.println("2nd arry = " + Arrays.toString(sortedHand));
+
+        return Integer.parseInt(sortedHand[sortedHand.length - 1]);
     }
 }
